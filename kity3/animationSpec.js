854a33cf83962f9778668dd58c17f2526d7eb1ed
Yang Hwan-Hee <muri1004@naver.com>
2017-10-05 01:24:31 +0900

    Redefined animationSpec.js
    - Rename animation.js to animationSpec.js.

---
+++ kity3/animationSpec.js
@@ -0,0 +1,19 @@
+/**
+ * AnimationSpec module for kinetic typography.
+ * 
+ * AnimationSpec is animation specification and it is used when creating Animation.
+ * AnimationSpec has some default motion specifications. So, You can use this.
+ * You can also make custom specification by description.
+ *
+ * @class AnimationSpec
+ */
+
+function AnimationSpec () {
+
+    // TODO:
+    // 1) rotationX,Y,Z
+    // 2) scale
+    // 3) 곡선
+    // 4) curve
+    // move
+}
