fbfad6880fc5edc2d5e376ce31ee2ce3897764fc
Yang Hwan-Hee <muri1004@naver.com>
2017-10-06 15:42:51 +0900

    Added testkt4.js

---
+++ testkt4.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    var text1 = "밤새 모니터에";
+    var textObjs = KITY.createText(text1, 'parent', 'text1', 'word',/* {
+        fontSize: 30,
+    }*/);
+
+    var player = KITY.getPlayer();
+    var container = new Container('container');
+    for (var i=0; i<textObjs.length; i++) {
+        var toX = 0;
+        if (i!=0) toX = 30*textObjs[i-1].text.length;
+        KITY.setStyle(textObjs[i].id, {fontSize: 30});
+        var spec = {
+            x: {0: 100+toX, delay: 0, duration: 1},
+            y: 200,
+            opacity: {0: 1, duration: 1}
+        }
+        KITY.setAnimationSpec(textObjs[i], spec);
+        var animation = KITY.createAnimation(textObjs[i]);
+        player.add(animation);
+        container.add(textObjs[i]);
+    }
+    KITY.setAnimationSpec(container, {
+        opacity: {1: 0, duration: 1},
+        y: {0: -200, duration: 1}
+    });
+    player.add(KITY.createAnimation(container));
+    player.play({mode: 'sequence'});
+}
